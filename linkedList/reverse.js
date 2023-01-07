3ff479ae6fa55767f91b56a181ba7142db635db2
satendra kushwaha <satendra@nimapinfotech.com>
2023-01-07 17:02:53 +0530

    linkedList

--- linkedList/reverse.js
+++ linkedList/reverse.js
@@ -1,52 +1,51 @@
-class Node{
-    constructor(data,next){
-        this.data=data;
-        this.next=null;
-    }
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-class singlyLinkedList{
-    constructor(data,next){
-        this.head=null;
-    }
+class singlyLinkedList {
+  constructor(data, next) {
+    this.head = null;
+  }
 
-    insertayBegining(data){
-        const node=new Node()
-        node.data=data;
-        if(!this.head){
-            this.head=node;
-        }else{
-            node.next=this.head
-            this.head=node
-        }
+  insertayBegining(data) {
+    const node = new Node();
+    node.data = data;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
     }
+  }
 
-    reverse(){
-        let headref=null
-        let temp=null
-        for(let i=this.head;i!=null;i=i.next){
-            headref=this.head
-            this.head=this.head.next
-            headref.next=temp
-            temp=headref
-        }
+  reverse() {
+    let headref = null;
+    let temp = null;
+    for (let i = this.head; i != null; i = i.next) {
+      headref = this.head;
+      this.head = this.head.next;
+      headref.next = temp;
+      temp = headref;
     }
-    
+  }
 
-    print(data){
-        for(let i=this.head;i!=null;i=i.next){
-            console.log(i.data)
-        }
+  print(data) {
+    for (let i = this.head; i != null; i = i.next) {
+      console.log(i.data);
     }
+  }
 }
 
-const sl=new singlyLinkedList
-sl.insertayBegining(6)
-sl.insertayBegining(5)
-sl.insertayBegining(4)
+const sl = new singlyLinkedList();
+sl.insertayBegining(6);
+sl.insertayBegining(5);
+sl.insertayBegining(4);
 
-sl.insertayBegining(3)
-sl.insertayBegining(2)
-sl.insertayBegining(1)
-sl.reverse()
-sl.print()
+sl.insertayBegining(3);
+sl.insertayBegining(2);
+sl.insertayBegining(1);
+sl.reverse();
+sl.print();
